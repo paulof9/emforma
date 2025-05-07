d0552240a0d0cbb15437c6767cb8057a3c844a9a
paulof9 <above2@outlook.com>
2025-05-07 08:47:01 -0300

    Move função de ordem alfabética para a página principal

--- app/page.tsx
+++ app/page.tsx
@@ -72,16 +72,35 @@ export default function Home() {
   const indiceInicial = (paginaAtual - 1) * itensPorPagina;
   const produtosPaginados = produtosFiltrados.slice(indiceInicial, indiceInicial + itensPorPagina);
 
+
+  useEffect(() => {
+    async function loadProdutos() {
+      try {
+        const response = await fetch('/api/produtos');
+        const produtosData: ProdutoItem[] = await response.json();
+        setProdutos(produtosData);
+      } catch (error) {
+        console.error('Erro ao carregar produtos:', error);
+      }
+    }
+
+    loadProdutos();
+  }, []);
+
+  // Ordenação em ordem alfabetica por padrão
+  const produtosOrdenados = [...produtos].sort((a, b) =>
+    (a.nome || '').toLowerCase().localeCompare((b.nome || '').toLowerCase())
+  );
+
+  const produtosFiltrados = produtosOrdenados.filter((produto) =>
+    produto.nome.toLowerCase().includes(busca.toLowerCase())
+  );
+
   return (
     <main className="flex flex-col items-center bg-gray-50 min-h-screen p-6">
       <BuscaInput busca={busca} setBusca={setBusca} />
       <h1 className="text-4xl font-bold mb-6 text-black">Produtos</h1>
-      <Card produtos={produtosPaginados} />
-      <Paginacao
-        paginaAtual={paginaAtual}
-        setPaginaAtual={setPaginaAtual}
-        totalPaginas={totalPaginas}
-      />
+      <Card busca={busca} />
     </main>
   );
 }
